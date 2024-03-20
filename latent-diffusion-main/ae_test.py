3f3fd667f4968a6e19eeeaf2634396e3862c74ab
Cam <cjen@ualberta.ca>
2024-03-19 20:53:01 -0600

    Working ae_test.py Mar19

--- latent-diffusion-main/ae_test.py
+++ latent-diffusion-main/ae_test.py
@@ -7,6 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from geotiff_process import image_read_custom, image_read, get_rgb
 
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 def load_model_from_config(config, ckpt):
     print(f"Loading model from {ckpt}")
@@ -14,49 +17,54 @@ def load_model_from_config(config, ckpt):
     global_step = pl_sd["global_step"]
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
+    print(model)
+    m, u = model.load
     model.cuda()
     model.eval()
 
     return model
 
 
-directory_path = "D:/drive/research_uofa/LDM_exps/CTGAN/CTGAN/Sen2_MTC/dataset/Sen2_MTC/T09WWP_R071/cloudless/"
-image_name = "T09WWP_R071_0.tif"
+# directory_path = "D:/drive/research_uofa/LDM_exps/CTGAN/CTGAN/Sen2_MTC/dataset/Sen2_MTC/T09WWP_R071/cloudless/"
+# image_name = "T09WWP_R071_0.tif"
 
 # im_original = image_read_custom(directory_path + "/" + image_name)[:, :, 0:3]
 # im = im_original.reshape(1, 3, 256, 256)
-im_original = image_read(directory_path + "/" + image_name)
-im_original = get_rgb(im_original)
+# im_original = image_read(directory_path + "/" + image_name)
+# im_original = get_rgb(im_original)
 
-im = im_original.reshape(1, 3, 256, 256)
-print(im.shape)
-im = torch.from_numpy(im).to("cuda:0")
+# im = im_original.reshape(1, 3, 256, 256)
+# print(im.shape)
+# im = torch.from_numpy(im).to("cuda:0")
 
-lena_original = Image.open("lena.png")
-
+lena_original = Image.open("lena.png") 
 lena = lena_original.resize((256, 256), Image.Resampling.LANCZOS)
-print(np.array(lena).shape)
+lena = torch.from_numpy(np.array(lena, dtype=np.float32)).to("cuda:0") / 255.0
 
-lena = torch.from_numpy(np.array(lena, dtype=np.float32)).to("cuda:0")
-lena = lena.reshape(1, 3, 256, 256) / 255.0
-
+lena = lena.permute(2, 0, 1)
+lena = lena.reshape(1, 3, 256, 256)
 
 path_conf = "models/first_stage_models/vq-f4/config.yaml"
 config = OmegaConf.load(path_conf)
-model = load_model_from_config(config, "../../model.ckpt")
+
+model = instantiate_from_config(config.model)
+model.load_state_dict(torch.load("../../model.ckpt")['state_dict'], strict=False)
+model = model.to("cuda:0")
 
 q, l, info = model.encode(lena)
-
 restored = model.decode(q)
 
 restored = restored.cpu()
-restored = restored.detach().numpy()
-print(restored)
+restored = restored.detach().numpy().transpose(0, 2, 3, 1)[0]
+print(restored.shape)
 
+lena = lena.cpu()
+lena = lena.detach().numpy().transpose(0, 2, 3, 1)[0]
 
 plt.subplot(1,2,1)
-plt.imshow(lena_original)
+# lena = lena.reshape(256, 256, 3).cpu()
+plt.imshow(lena)
 plt.subplot(1,2,2)
-plt.imshow(restored.reshape(256, 256, 3))
+# restored = restored.reshape(256, 256, 3)
+plt.imshow(restored)
 plt.show()
